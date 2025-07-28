61619cb7fde30c2ce514dab13ad19891f654633a
Surve <surve8729@gmail.com>
2025-07-28 15:33:44 +0530

    CRUD Operations

--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using EFGetStarted;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
 Console.WriteLine("Hello, World!");
+
+using var db = new BloggingContext();
+
+Console.WriteLine($"Database Path: {db.DbPath}");
+
+// Create
+Console.WriteLine("Inserting a New Blog...");
+db.Add(new BloggingContext.Blog
+{
+    Url = "http://bhenchod.com"
+});
+await db.SaveChangesAsync();
+
+// Read
+Console.WriteLine("Reading 1st Blog...");
+var blog = await db.Blogs
+    .Include(b => b.Posts)
+    .OrderBy(b => b.BlogId)
+    .FirstAsync();
+Console.WriteLine(blog.Url);
+
+// Update
+Console.WriteLine("Updating 1st Blog...");
+blog.Url = "https://madarchod.com";
+blog.Posts.Add(new BloggingContext
+    .Post
+    {
+        Title = "Hello Guys", 
+        Content = "Bitchless guy"
+    });
+await db.SaveChangesAsync();
+Console.WriteLine(blog.Url);
+
+// Read Posts
+Console.WriteLine("Reading Posts from 1st Blog...");
+foreach (var post in blog.Posts)
+{
+   Console.WriteLine(post.Title);
+   Console.WriteLine(post.Content);
+}
+
+// Delete
+Console.WriteLine("Deleting the 1st blog...");
+db.Remove(blog);
+await db.SaveChangesAsync();
--- obj/Debug/net8.0/EFGetStarted.AssemblyInfo.cs
+++ obj/Debug/net8.0/EFGetStarted.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EFGetStarted")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29d3ecf337dd15d78533c55a261ba45df8a8c6bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2c8fd93f6ab72f4faa62f455e40752c663f88ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("EFGetStarted")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EFGetStarted")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
